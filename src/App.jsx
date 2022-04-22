8b4dc9fde2cab11df42ffc40bdac8fe32592d83b
DinhTuanAnh2k1 <dinhtuananh12072001@gmail.com>
2022-04-22 22:33:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+
+
+function App() {
+  return (
+    <div className="trello-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorn</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+              Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+            <li>fifth</li>
+            <li>sixth</li>
+            <li>seventh</li>
+            <li>eighth</li>
+            <li>thirteenth</li>
+            <li>eighth</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorn</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+              Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+            
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorn</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+              Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+            <li>fifth</li>
+            <li>sixth</li>
+            
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorn</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+              Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorn</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+              Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+            <li>fifth</li>
+            
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorn</header>
+          <ul>
+            <li>
+              <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png"></img>
+              Design & Research
+            </li>
+            <li>second</li>
+            
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
